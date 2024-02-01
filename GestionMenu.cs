8c349674e106ab0af6414a951d99007102083ab9
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2024-02-01 17:14:50 +0100

    début implémentation de la gestion du choix de l'utilisateur dans le menu

--- GestionMenu.cs
+++ GestionMenu.cs
@@ -8,19 +8,19 @@ namespace ExerciceC__GestionnaireCompteBancaire
 {
     internal class GestionMenu
     {
-        public static void AfficherMenu() 
+        public static void AfficherMenu()
         {
 
             while (true)
             {
                 Console.WriteLine("Appuyez sur Entrée pour afficher le menu.");
-                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
                 if (keyInfo.Key == ConsoleKey.Enter)
-                    {
+                {
                     Console.WriteLine
                         (
-                        "[I] Voir les informations sur le titulaire du compte;" + Environment.NewLine +
+                        "[I] Voir les informations sur le titulaire du compte" + Environment.NewLine +
                         "[CS] Compte courant - Consulter le solde" + Environment.NewLine +
                         "[CD] Compte courant - Déposer des fonds" + Environment.NewLine +
                         "[CR] Compte courant - Retirer des fonds" + Environment.NewLine +
@@ -29,10 +29,46 @@ namespace ExerciceC__GestionnaireCompteBancaire
                         "[ER] Compte épargne - Retirer des fonds" + Environment.NewLine +
                         "[X] Quitter"
                         );
+                    string saisieUtilisateur = Console.ReadLine();
+                    GestionChoixUtilisateur(saisieUtilisateur);
                     break;
                 }
             }
         }
-        
+
+        public static void GestionChoixUtilisateur(string saisieUtilisateur)
+        {
+            switch (saisieUtilisateur)
+            {
+                case "I":
+                    Console.WriteLine("Voir les informations sur le titulaire du compte");
+                    break;
+                case "CS":
+                    Console.WriteLine("Compte courant - Consulter le solde");
+                    break;
+                case "CD":
+                    Console.WriteLine("Compte courant - Déposer des fonds");
+                    break;
+                case "CR":
+                    Console.WriteLine("Compte courant - Retirer des fonds");
+                    break;
+                case "ES":
+                    Console.WriteLine("Compte épargne - Consulter le solde");
+                    break;
+                case "ED":
+                    Console.WriteLine("Compte épargne - Déposer des fonds");
+                    break;
+                case "ER":
+                    Console.WriteLine("Compte épargne - Retirer des fonds");
+                    break;
+                case "X":
+                    Console.WriteLine("Quitter");
+                    break;
+                default:
+                    Console.WriteLine("Veuillez saisir un choix possible dans le menu");
+                    AfficherMenu();
+                    break;
+            }
+        }
     }
 }
